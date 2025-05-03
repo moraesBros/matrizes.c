ea6a9eec0390fa6b8331bbef60013f6e2fd92443
moraesBros <161986858+moraesBros@users.noreply.github.com>
2025-05-02 23:57:44 -0300

    Create puts.c

---
+++ puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char mensagem[] = "Bem-vindo ao programa!";
+
+    // Exibindo a mensagem com puts()
+    puts(mensagem);
+
+    // Exemplo de uso direto da string
+    puts("Este é um exemplo prático do uso de puts().");
+
+    return 0;
+}
